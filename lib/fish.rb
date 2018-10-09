b27bf743d2999d291bddc53bd14a2add055ca7d1
Manny Pissanos <mpissano@gmail.com>
2018-10-09 18:42:51 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,6 +1,18 @@
 class Fish
-  attr_reader :name, :species
+  attr_reader :name
   attr_accessor :mood
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+end
+
+
+class Fish
+  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
   def initialize(name)
     @name = name
     @mood = "nervous"
