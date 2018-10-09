db7f8a1476d2d772df0e0a0921e52a36d459e4f8
Manny Pissanos <mpissano@gmail.com>
2018-10-09 18:49:36 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -39,14 +39,4 @@ class Owner
 end
 
 
-  def buy_fish(fish_name)
-    @pets[:fishes] << Fish.new(fish_name)
-  end
-
-  def buy_cat(cat_name)
-    @pets[:cats] << Cat.new(cat_name)
-  end
-
-  def buy_dog(dog_name)
-    @pets[:dogs] << Dog.new(dog_name)
-  end
+ 
